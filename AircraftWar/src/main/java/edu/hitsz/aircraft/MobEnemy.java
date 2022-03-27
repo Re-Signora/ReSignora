ae9c06eb89035b2e4a520b15aaf086c07ab5782c
Chiro Liang <Chiro2001@163.com>
2022-03-27 16:27:01 +0800

    add base files

---
+++ AircraftWar/src/main/java/edu/hitsz/aircraft/MobEnemy.java
@@ -0,0 +1,36 @@
+package edu.hitsz.aircraft;
+
+import edu.hitsz.application.Game;
+import edu.hitsz.application.Main;
+import edu.hitsz.bullet.AbstractBullet;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 普通敌机
+ * 不可射击
+ *
+ * @author hitsz
+ */
+public class MobEnemy extends AbstractAircraft {
+
+    public MobEnemy(int locationX, int locationY, int speedX, int speedY, int hp) {
+        super(locationX, locationY, speedX, speedY, hp);
+    }
+
+    @Override
+    public void forward() {
+        super.forward();
+        // 判定 y 轴向下飞行出界
+        if (locationY >= Main.WINDOW_HEIGHT ) {
+            vanish();
+        }
+    }
+
+    @Override
+    public List<AbstractBullet> shoot() {
+        return new LinkedList<>();
+    }
+
+}
