828be0c3580fa2c2975fee546d686231ba273e6b
Chiro Liang <Chiro2001@163.com>
2022-04-01 19:56:16 +0800

    add fps limit

--- src/main/scala/work/chiro/game/application/Game.scala
+++ src/main/scala/work/chiro/game/application/Game.scala
@@ -115,11 +115,11 @@ class Game(frame: JFrame) extends JPanel {
         if (onMobCreateCountCycle) enemyAircrafts.synchronized(enemyAircrafts.append(MobEnemy.create()))
         // 产生精英敌机
         if (onEliteCreateCountCycle) enemyAircrafts.synchronized(enemyAircrafts.append(EliteEnemy.create()))
-        // if (onFpsCountCycle) {
-        //   val fpsInfo = f"[ Calc ${frameCalcCount.size} fps | Render ${frameRenderCount.size} fps ]"
-        //   if (config.running.showFps) logger.info(fpsInfo)
-        //   Main.getFrameInstance.setTitle(f"Aircraft War $fpsInfo")
-        // }
+        if (onFpsCountCycle) {
+          val fpsInfo = f"[ Calc ${frameCalcCount.size} fps | Render ${frameRenderCount.size} fps ]"
+          if (config.running.showFps) logger.info(fpsInfo)
+          Main.getFrameInstance.setTitle(f"Aircraft War $fpsInfo")
+        }
         // 所有物体移动
         allObjectLists.foreach(_.foreach(_.forward()))
         // 撞击检测
