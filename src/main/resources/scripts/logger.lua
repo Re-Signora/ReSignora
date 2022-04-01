d4928dad9a4a019e00a4c492d5763b90e5e673f5
Chiro Liang <Chiro2001@163.com>
2022-04-01 15:36:57 +0800

    fasten lua logger

--- src/main/resources/scripts/logger.lua
+++ src/main/resources/scripts/logger.lua
@@ -2,22 +2,22 @@ require "logging"
 
 logger = {
     log = function(value)
-        logging.log(value, debug.getinfo(2))
+        logging.log(value, debug.getinfo(2, "nSl"))
     end,
     debug = function(value)
-        logging.debug(value, debug.getinfo(2))
+        logging.debug(value, debug.getinfo(2, "nSl"))
     end,
     info = function(value)
-        logging.info(value, debug.getinfo(2))
+        logging.info(value, debug.getinfo(2, "nSl"))
     end,
     warn = function(value)
-        logging.warn(value, debug.getinfo(2))
+        logging.warn(value, debug.getinfo(2, "nSl"))
     end,
     error = function(value)
-        logging.error(value, debug.getinfo(2))
+        logging.error(value, debug.getinfo(2, "nSl"))
     end,
     fatal = function(value)
-        logging.fatal(value, debug.getinfo(2))
+        logging.fatal(value, debug.getinfo(2, "nSl"))
     end,
 
 }
