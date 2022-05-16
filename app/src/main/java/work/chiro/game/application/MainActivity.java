3cea93dd98b7c09475be2babf97163fb114a1682
Chiro <Chiro2001@163.com>
2022-05-17 05:46:27 +0800

    call GameActivity in MainActivity

--- app/src/main/java/work/chiro/game/application/MainActivity.java
+++ app/src/main/java/work/chiro/game/application/MainActivity.java
@@ -1,6 +1,8 @@
 package work.chiro.game.application;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, GameActivity.class);
+            startActivity(intent);
+        });
     }
 }
