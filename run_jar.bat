4d46172dd7d7c244b649c9aec452d2e372be0491
Chiro <Chiro2001@163.com>
2022-05-17 16:24:21 +0800

    update run_jar.bat

--- run_jar.bat
+++ run_jar.bat
@@ -1,1 +1,2 @@
-java -jar app\build\libs\app.jar
+gradlew :pc:jar
+java -Dfile.encoding=UTF-8 -Duser.country=CN -Duser.language=zh -Duser.variant -cp core\build\libs\core.jar;pc\build\libs\pc.jar work.chiro.game.application.Main
