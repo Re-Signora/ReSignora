235f663fe31b5d90fa9cd77ce538ca4d50b56138
Chiro <Chiro2001@163.com>
2022-05-24 16:08:34 +0800

    add two activity

---
+++ core/src/main/java/work/chiro/game/xactivity/DialogActivity.java
@@ -0,0 +1,10 @@
+package work.chiro.game.xactivity;
+
+import work.chiro.game.game.Game;
+import work.chiro.game.x.activity.XActivity;
+
+public class DialogActivity extends XActivity {
+    public DialogActivity(Game game) {
+        super(game);
+    }
+}
---
+++ core/src/main/java/work/chiro/game/xactivity/FightActivity.java
@@ -0,0 +1,10 @@
+package work.chiro.game.xactivity;
+
+import work.chiro.game.game.Game;
+import work.chiro.game.x.activity.XActivity;
+
+public class FightActivity extends XActivity {
+    public FightActivity(Game game) {
+        super(game);
+    }
+}
