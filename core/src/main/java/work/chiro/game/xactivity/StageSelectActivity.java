95817ae4134201988447f526147eefc1cb3e304b
Chiro <Chiro2001@163.com>
2022-05-29 20:11:54 +0800

    finish ax xActivity

--- core/src/main/java/work/chiro/game/xactivity/StageSelectActivity.java
+++ core/src/main/java/work/chiro/game/xactivity/StageSelectActivity.java
@@ -3,6 +3,7 @@ package work.chiro.game.xactivity;
 import work.chiro.game.game.Game;
 import work.chiro.game.x.activity.XActivity;
 import work.chiro.game.x.activity.XBundle;
+import work.chiro.game.x.ui.XButton;
 
 public class StageSelectActivity extends XActivity {
     public StageSelectActivity(Game game) {
@@ -13,5 +14,7 @@ public class StageSelectActivity extends XActivity {
     protected void onCreate(XBundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView("stageSelect");
+        XButton buttonStage02 = (XButton) findViewById("buttonStage02");
+        buttonStage02.setOnClick((xView, xEvent) -> finish());
     }
 }
