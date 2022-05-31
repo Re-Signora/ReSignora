a9cc7100f1ed209bc636817ea2231e8d1f95e08d
Chiro <Chiro2001@163.com>
2022-05-31 17:47:08 +0800

    add attack interface

--- core/src/main/java/work/chiro/game/objects/thing/character/AbstractCharacter.java
+++ core/src/main/java/work/chiro/game/objects/thing/character/AbstractCharacter.java
@@ -1,8 +1,11 @@
 package work.chiro.game.objects.thing.character;
 
+import java.util.List;
+
 import work.chiro.game.animate.action.AbstractAction;
 import work.chiro.game.logic.attributes.BasicCharacterAttributes;
 import work.chiro.game.objects.thing.AbstractThing;
+import work.chiro.game.objects.thing.attack.AbstractAttack;
 import work.chiro.game.vector.Scale;
 import work.chiro.game.vector.Vec2;
 
@@ -13,5 +16,17 @@ public class AbstractCharacter extends AbstractThing<BasicCharacterAttributes, A
 
     public AbstractCharacter(String labelName, Class<BasicCharacterAttributes> attributesClass, Vec2 posInit, AbstractAction animateContainer) {
         this(labelName, attributesClass, posInit, animateContainer, null, null, null);
+    }
+
+    public List<AbstractAttack> normalAttack() {
+        return List.of();
+    }
+
+    public List<AbstractAttack> skillAttack() {
+        return List.of();
+    }
+
+    public List<AbstractAttack> chargedAttack() {
+        return List.of();
     }
 }
