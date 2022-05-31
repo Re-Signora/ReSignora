358434f14a01d6096757b6a31f6f857e78583167
Chiro <Chiro2001@163.com>
2022-05-31 11:46:25 +0800

    draw circle in XJaySticks

--- core/src/main/java/work/chiro/game/x/ui/view/XJoySticks.java
+++ core/src/main/java/work/chiro/game/x/ui/view/XJoySticks.java
@@ -1,9 +1,18 @@
 package work.chiro.game.x.ui.view;
 
 import work.chiro.game.vector.Vec2;
+import work.chiro.game.x.compatible.DrawColor;
+import work.chiro.game.x.compatible.XGraphics;
 
 public class XJoySticks extends XView {
     public XJoySticks(Vec2 posInit) {
         super(posInit);
+    }
+
+    @Override
+    public void draw(XGraphics g, boolean center) {
+        super.draw(g, center);
+        g.setColor(DrawColor.green);
+        g.circle(getLocationX() + getWidth() / 2, getLocationY() + getHeight() / 2, Math.min(getWidth(), getHeight()) / 2);
     }
 }
