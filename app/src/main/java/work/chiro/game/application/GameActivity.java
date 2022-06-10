68aa23798fa6f9a16956cd7c672d6afe79cae5dc
Chiro <Chiro2001@163.com>
2022-06-10 20:29:50 +0800

    fix: black screen

--- app/src/main/java/work/chiro/game/application/GameActivity.java
+++ app/src/main/java/work/chiro/game/application/GameActivity.java
@@ -9,9 +9,7 @@ import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AlertDialog;
@@ -52,8 +50,6 @@ public class GameActivity extends AppCompatActivity {
         } else {
             int windowWidth = surfaceView.getMeasuredWidth();
             int windowHeight = surfaceView.getMeasuredHeight();
-            // int windowWidth = 2400;
-            // int windowHeight = 1080;
             canvas.translate((windowWidth * 1.0f - RunningConfig.windowWidth * XGraphicsAndroid.getCanvasScale()) / 2,
                     (windowHeight * 1.0f - RunningConfig.windowHeight * XGraphicsAndroid.getCanvasScale()) / 2);
             canvas.scale(XGraphicsAndroid.getCanvasScale(), XGraphicsAndroid.getCanvasScale());
@@ -84,12 +80,10 @@ public class GameActivity extends AppCompatActivity {
 
     private synchronized void draw() {
         XGraphicsAndroid xGraphics = getXGraphics();
-        // canvas.drawColor(Color.BLACK);
         xGraphics.getCanvas().drawColor(Color.BLACK);
         xGraphics.paintInOrdered(game);
         xGraphics.paintInfo(game);
         // 恢复 canvas 设置之后绘制遮罩
-        // canvas.restore();
         surfaceHolder.unlockCanvasAndPost(xGraphics.getCanvas());
     }
 
@@ -135,7 +129,7 @@ public class GameActivity extends AppCompatActivity {
 
         Game.clearInstance();
         game = Game.createInstance(heroControllerAndroid);
-        // game.setOnExit(this::finish);
+        game.setOnExit(this::finish);
 
         game.setOnPaint(() -> {
             if (surfaceHolder == null) {
@@ -198,8 +192,6 @@ public class GameActivity extends AppCompatActivity {
         UtilsAndroid.setScreen(this);
         int windowWidth = surfaceView.getMeasuredWidth();
         int windowHeight = surfaceView.getMeasuredHeight();
-        // int windowWidth = 2400;
-        // int windowHeight = 1080;
 
         if (RunningConfig.allowResize) {
             RunningConfig.windowWidth = windowWidth;
@@ -218,56 +210,27 @@ public class GameActivity extends AppCompatActivity {
 
     private void resetGame() {
         resetScale();
-        // game.resetStates();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        // MyThreadFactory.getInstance().newThread(() -> {
         UtilsAndroid.setScreen(this);
-        // try {
-        //     Thread.sleep(1000);
-        // } catch (InterruptedException e) {
-        //     e.printStackTrace();
-        // }
-
-        //     resetGame();
-        //     game.action();
-        //     HistoryImplAndroid.getInstance(this);
-        // }).start();
-        // try {
-        //     Thread.sleep(300);
-        // } catch (InterruptedException e) {
-        //     e.printStackTrace();
-        // }
         resetGame();
-        MyThreadFactory.getInstance().newThread(() -> game.action(), "StartGame").start();
-        // game.action();
+        MyThreadFactory.getInstance().newThread(() -> {
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            game.action();
+        }, "StartGame").start();
     }
 
     @Override
     protected void onStop() {
         resetGame();
         super.onStop();
-    }
-
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        // 隐藏导航条 | 设置页面全屏显示
-        View decorView = getWindow().getDecorView();
-        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
-        decorView.setSystemUiVisibility(uiOptions);
-        // 不息屏
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        // 显示区域扩展到刘海
-        WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
-        layoutParams.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
-        getWindow().setAttributes(layoutParams);
     }
 
     @Override
