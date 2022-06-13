7edb6369dc22f18c6ab1af1d32a8424962b1d62c
Chiro <Chiro2001@163.com>
2022-06-13 22:15:05 +0800

    adding moving animate

--- core/src/main/java/work/chiro/game/objects/thing/character/shogunate/ShogunateSoldier.java
+++ core/src/main/java/work/chiro/game/objects/thing/character/shogunate/ShogunateSoldier.java
@@ -3,28 +3,41 @@ package work.chiro.game.objects.thing.character.shogunate;
 import java.io.IOException;
 
 import work.chiro.game.animate.Animate;
+import work.chiro.game.animate.AnimateVectorType;
 import work.chiro.game.animate.action.AbstractAction;
 import work.chiro.game.game.Game;
 import work.chiro.game.logic.attributes.loadable.BasicCharacterAttributes;
 import work.chiro.game.objects.thing.character.AbstractCharacter;
 import work.chiro.game.utils.Utils;
 import work.chiro.game.utils.timer.DelayTimer;
+import work.chiro.game.utils.timer.TimeManager;
 import work.chiro.game.utils.timer.Timer;
 import work.chiro.game.vector.Scale;
 import work.chiro.game.vector.Vec2;
 import work.chiro.game.x.compatible.XImage;
 
+@SuppressWarnings("FieldCanBeLocal")
 public class ShogunateSoldier extends AbstractCharacter {
     private boolean normalAttacking = false;
-    private int normalAttackDuration = 1000;
+    private boolean moving = false;
+    private final int normalAttackDuration = 1000;
+    private double moveDuration = 0;
     private Timer normalAttackTask = null;
     private Timer moveTask = null;
 
     public ShogunateSoldier(Vec2 posInit, AbstractAction abstractAction, Vec2 sizeInit, Scale rotationInit, Scale alpha) {
         super("shogunate-soldier", BasicCharacterAttributes.class, posInit, abstractAction, sizeInit, rotationInit, alpha);
+        moveDuration = 100.0 / getBasicAttributes().getSpeed() * 1000;
         normalAttackTask = new Timer(getBasicAttributes().getNormalAttackCoolDown() * 1000, (controller, timer) -> this.normalAttack());
-        moveTask = new Timer(100.0 / getBasicAttributes().getSpeed(), (controller, timer) -> {});
+        moveTask = new Timer(moveDuration, (controller, timer) -> {
+            Vec2 delta = Game.getInstance().getObjectController().getTarget().getPosition().minus(getPosition());
+            Vec2 newPos = delta.fromVector(getPosition().plus(delta.divide(Math.sqrt(delta.getScale().getX())).times(getBasicAttributes().getSpeed() * 20)));
+            moving = true;
+            getAnimateContainer().addAnimate(new Animate.SmoothTo<>(getPosition(), newPos, AnimateVectorType.Others, TimeManager.getTimeMills(), moveDuration / 3
+            ).setAnimateCallback(animate -> moving = false));
+        });
         Game.getInstance().getTimerController().add(getClass(), normalAttackTask);
+        Game.getInstance().getTimerController().add(getClass(), moveTask);
     }
 
     public ShogunateSoldier(Vec2 posInit, AbstractAction abstractAction) {
@@ -42,7 +55,7 @@ public class ShogunateSoldier extends AbstractCharacter {
 
     @Override
     public XImage<?> getImage(boolean getRawImage) {
-        if (normalAttacking) {
+        if (normalAttacking || moving) {
             try {
                 return Utils.getCachedImageFromResource("characters/" + getLabelName() + "/attack.png");
             } catch (IOException e) {
